bb3ee5ce41c4c6dae07922f7e0f1c85019736342
Алексей <mr.nail.brain13@gmail.com>
2021-05-30 14:36:51 +0300

    Внес исправления по замечаниям.

--- js/main.js
+++ js/main.js
@@ -1,38 +1,14 @@
-//Задание №1
-function getNumber(minValue, maxValue) {
-  if (minValue >= 0) {
-    minValue = Math.ceil(minValue);
-    maxValue = Math.floor(maxValue);
-    return Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue;
-  }
-  return 'error message';
-}
+// eslint-disable-next-line
+'use strict';
 
-getNumber(10, 20);
-
-//Задание №2
-function getNumberFloat(minValue, maxValue, dotValue) {
-  if (minValue >= 0) {
+const getNumber = (minValue, maxValue, dotValue = 0) => {
+  if (maxValue >= minValue && minValue >= 0 && maxValue > 0) {
     minValue = Math.ceil(minValue);
     maxValue = Math.floor(maxValue);
     const randomFloat = Math.random() * (maxValue - minValue) + minValue;
     return randomFloat.toFixed(dotValue);
   }
-  return 'error message coordinate';
-}
-
-getNumberFloat(10, 20, 2);
-
-//источник https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-
-//Пример со стрелочными функциями
-const getNumberArw = (minValue, maxValue) => {
-  if (minValue >= 0) {
-    minValue = Math.ceil(minValue);
-    maxValue = Math.floor(maxValue);
-    return Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue;
-  }
   return 'error message';
 };
 
-getNumberArw(0, 20);
+getNumber(0, 20, 2);
