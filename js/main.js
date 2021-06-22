31859f5525d2843775d60800dff07e97ccec1e6b
Алексей <mr.nail.brain13@gmail.com>
2021-06-22 17:33:45 +0300

    Проверка валидации.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 import { toggleForm } from './modules/form.js';
 import './modules/map.js';
 import { generateCardsList } from './modules/similarobject.js';
+import './modules/validation.js';
 
 generateCardsList();
 
 //переключение true/false
-toggleForm(true);
+toggleForm(false);
