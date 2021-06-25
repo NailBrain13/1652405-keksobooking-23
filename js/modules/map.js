ba3b009b54c1f131bdfed966ced280e906ce8045
Алексей <mr.nail.brain13@gmail.com>
2021-06-25 15:29:05 +0300

    Добавление карточек на метки.

--- js/modules/map.js
+++ js/modules/map.js
@@ -1,22 +1,21 @@
 import { toggleForm } from './form.js';
 import { getNumber } from './util.js';
 import { arrayOffers } from './test-data.js';
-
+import { getCard } from './card.js';
 const latRandom = getNumber(35.65, 35.7, 5);
 const lngRandom = getNumber(139.7, 139.8, 5);
+const TEST_MARKERS = 10;
+const TOKYO_CENTER = {
+  lat: 35.68941,
+  lng: 139.69232,
+};
 const inputAddress = document.querySelector('#address');
 
 const map = L.map('map-canvas')
   .on('load', () => {
     toggleForm(false);
   })
-  .setView(
-    {
-      lat: 35.6952,
-      lng: 139.757,
-    },
-    10,
-  );
+  .setView(TOKYO_CENTER, 10);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution:
     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
@@ -33,51 +32,25 @@ const alterPinIcon = L.icon({
   iconAnchor: [20, 40],
 });
 
-const mainPinMarker = L.marker(
-  {
-    lat: 35.6952,
-    lng: 139.757,
-  },
-  {
-    draggable: true,
-    icon: mainPinIcon,
-  },
-);
+const mainPinMarker = L.marker(TOKYO_CENTER, {
+  draggable: true,
+  icon: mainPinIcon,
+});
 mainPinMarker.addTo(map);
 
-const alterPinMarker = L.marker(
-  {
-    lat: latRandom,
-    lng: lngRandom,
-  },
-  {
-    icon: alterPinIcon,
-  },
-);
-alterPinMarker.addTo(map);
-
-const searchArea = L.circle([35.6952, 139.757], {
+const searchArea = L.circle(TOKYO_CENTER, {
   color: 'black',
   fillColor: '#7FFFD4',
   fillOpacity: 0.5,
-  radius: 22000,
+  radius: 12000,
 });
 searchArea.addTo(map);
 
 const resetButton = document.querySelector('.ad-form__reset');
 resetButton.addEventListener('click', () => {
-  mainPinMarker.setLatLng({
-    lat: 35.6952,
-    lng: 139.757,
-  });
-  map.setView(
-    {
-      lat: 35.68954,
-      lng: 139.69171,
-    },
-    10,
-  );
-  alterPinMarker.remove();
+  mainPinMarker.setLatLng(TOKYO_CENTER);
+
+  map.setView(TOKYO_CENTER, 10);
   searchArea.remove();
 });
 
@@ -88,18 +61,22 @@ mainPinMarker.on('moveend', (evt) => {
   inputAddress.value = `${lat},  ${lng}`;
 });
 
-const testMarkers = arrayOffers(6);
-// console.log(testMarkers);
+const testMarkers = arrayOffers(TEST_MARKERS);
 
-testMarkers.forEach((item) => {
-  const marker = L.marker(
-    {
-      lat: item.offer.location.lat,
-      lng: item.offer.location.lng,
-    },
-    { icon: alterPinIcon },
-  );
+const createMarkers = () => {
+  testMarkers.forEach((obj) => {
+    const marker = L.marker(
+      {
+        lat: obj.offer.location.lat,
+        lng: obj.offer.location.lng,
+      },
+      { icon: alterPinIcon },
+    );
 
-  marker.addTo(map);
-});
-export {};
+    marker.addTo(map).bindPopup(getCard(obj), {
+      keepInView: true,
+    });
+  });
+};
+
+export { createMarkers };
