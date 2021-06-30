8687390a7df4cbf69d25677c72b6589b4920a5e0
Алексей <mr.nail.brain13@gmail.com>
2021-06-30 18:46:47 +0300

    Rename

--- js/modules/messages.js
+++ js/modules/messages.js
@@ -7,17 +7,17 @@ const notifeField = document.querySelector('.notice');
 const ALERT_SHOW_TIME = 3000;
 
 const getMessage = (value) => {
-  const Massege = value.cloneNode(true);
+  const messagePopUp = value.cloneNode(true);
 
-  Massege.style.zIndex = 100;
-  Massege.style.position = 'sticky';
-  Massege.style.top = 0;
-  Massege.style.bottom = 0;
+  messagePopUp.style.zIndex = 100;
+  messagePopUp.style.position = 'sticky';
+  messagePopUp.style.top = 0;
+  messagePopUp.style.bottom = 0;
 
-  notifeField.append(Massege);
+  notifeField.append(messagePopUp);
 
   setTimeout(() => {
-    Massege.remove();
+    messagePopUp.remove();
   }, ALERT_SHOW_TIME);
 };
 
