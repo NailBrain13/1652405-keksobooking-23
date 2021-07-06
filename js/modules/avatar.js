da666fca2874f127b420c37d020144d6a092b565
Алексей <mr.nail.brain13@gmail.com>
2021-07-06 13:34:56 +0300

    Пользовательское фото.

--- js/modules/avatar.js
+++ js/modules/avatar.js
@@ -1,7 +1,13 @@
 const FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
 const AVATAR_DEF = 'img/muffin-grey.svg';
+const IMG_SIZE = '70';
 const fileChooser = document.querySelector('.ad-form__field input[type=file]');
-const preview = document.querySelector('.ad-form-header__preview img');
+const previewAvatar = document.querySelector('.ad-form-header__preview img');
+const previevContainer = document.querySelector('.ad-form__photo-container');
+const previevPhoto = document.querySelector('.ad-form__photo');
+const previevUpload = document.querySelector(
+  '.ad-form__upload input[type=file]',
+);
 
 fileChooser.addEventListener('change', () => {
   const file = fileChooser.files[0];
@@ -13,7 +19,40 @@ fileChooser.addEventListener('change', () => {
     const reader = new FileReader();
 
     reader.addEventListener('load', () => {
-      preview.src = reader.result;
+      previewAvatar.src = reader.result;
+    });
+
+    reader.readAsDataURL(file);
+  }
+});
+
+const clearImages = () => {
+  Array.from(previevPhoto.children).forEach((img) => {
+    img.remove();
+  });
+};
+
+const addImg = (result) => {
+  const div = previevPhoto;
+  const img = previewAvatar.cloneNode(true);
+  img.src = result;
+  img.width = IMG_SIZE;
+  img.height = IMG_SIZE;
+  div.appendChild(img);
+  previevContainer.appendChild(div);
+};
+
+previevUpload.addEventListener('change', () => {
+  const file = previevUpload.files[0];
+  const fileName = file.name.toLowerCase();
+
+  const matches = FILE_TYPES.some((it) => fileName.endsWith(it));
+
+  if (matches) {
+    const reader = new FileReader();
+
+    reader.addEventListener('load', () => {
+      addImg(reader.result);
     });
 
     reader.readAsDataURL(file);
@@ -21,7 +60,8 @@ fileChooser.addEventListener('change', () => {
 });
 
 const resetAvatar = () => {
-  preview.src = AVATAR_DEF;
+  previewAvatar.src = AVATAR_DEF;
+  clearImages();
 };
 
 export { resetAvatar };
